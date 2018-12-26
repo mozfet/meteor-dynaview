9669229917b13af101efc214b5d47ad2643090b8
Mozfet <wynand@expertbox.com>
2018-12-26 10:37:55 +0200

    initial commit

--- package.js
+++ package.js
@@ -1,24 +1,32 @@
 Package.describe({
-  name: 'meteor-dynaview',
+  name: 'dynaview',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Reactively rendered views and MaterializeCSS modals.',
+  git: 'https://github.com/mozfet/meteor-dynaview.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.8.0.1');
-  api.use('ecmascript');
-  api.mainModule('meteor-dynaview.js');
+  api.use([
+    'ecmascript',
+    'templating',
+    'session'
+  ], 'client');
+  api.addFiles([
+    './dynaView/dynaView.html',
+    './dynaView/dynaView.js',
+    './dynaView/dynaViewLauncher.html',
+    './dynaView/dynaViewLauncher.js',
+    './dynaView/dynaViewMaterialModal/dynaViewMaterialModal.html',
+    './dynaView/dynaViewMaterialModal/dynaViewMaterialModal.js'    
+  ], 'client', { lazy: true })
+  // api.mainModule('meteor-dynaview.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('meteor-dynaview');
-  api.mainModule('meteor-dynaview-tests.js');
-});
+// Package.onTest(function(api) {
+//   api.use('ecmascript');
+//   api.use('tinytest');
+//   api.use('meteor-dynaview');
+//   api.mainModule('meteor-dynaview-tests.js');
+// });
